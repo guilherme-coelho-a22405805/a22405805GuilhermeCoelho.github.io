399b8cd516b918b4f59864eda534e6bbe43a3f46
guilhermecoelho <a22405805@alunos.ulht.pt>
2025-10-23 10:56:35 +0000

    Prepara para começar o lab7

--- lab6/produtos.js
+++ lab6/produtos.js
@@ -146,14 +146,14 @@ const produtos = [
 ]
 
 
-// --- Seletores ---
+
 const container = document.getElementById('lista-produtos');
 const carrinhoLista = document.getElementById('carrinho-lista');
 const totalElement = document.getElementById('total');
 
 let total = 0;
 
-// --- Criação dos produtos ---
+
 produtos.forEach(produto => {
   const artigo = document.createElement('article');
   artigo.innerHTML = `
@@ -166,7 +166,6 @@ produtos.forEach(produto => {
     <button class="add">+ Adicionar ao Cesto</button>
   `;
 
-  // Evento do botão de adicionar
   const botao = artigo.querySelector('.add');
   botao.addEventListener('click', () => {
     adicionarAoCesto(produto);
@@ -175,7 +174,7 @@ produtos.forEach(produto => {
   container.appendChild(artigo);
 });
 
-// --- Função para adicionar ao carrinho ---
+
 function adicionarAoCesto(produto) {
   const item = document.createElement('li');
   item.innerHTML = `
@@ -193,14 +192,15 @@ function adicionarAoCesto(produto) {
   });
 }
 
-// --- Função para remover ---
+
 function removerDoCesto(item, preco) {
   item.remove();
   total -= preco;
   atualizarTotal();
 }
 
-// --- Atualizar total ---
+
 function atualizarTotal() {
   totalElement.textContent = ` Total: ${total.toFixed(2)} €`;
 }
+
