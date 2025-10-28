1d2076e4d2c171c35cd3002a291a74f46277ce31
guilhermecoelho <a22405805@alunos.ulht.pt>
2025-10-28 18:58:44 +0000

    1

--- lab7/scripts.js
+++ lab7/scripts.js
@@ -1,58 +1,15 @@
-// --- Seletores ---
-const container = document.getElementById('lista-produtos');
-const carrinhoLista = document.getElementById('carrinho-lista');
-const totalElement = document.getElementById('total');
+const url = "https://deisishop.pythonanywhere.com/products/";
+const section = document.querySelector("#lista-produtos");
 
-let total = 0;
+fetch(url)
+  .then(response => response.json())
+  .then(data =>{
+     console.log(data)
 
-// --- Criação dos produtos ---
-produtos.forEach(produto => {
-  const artigo = document.createElement('article');
-  artigo.innerHTML = `
-    <figure>
-      <img src="${produto.image}" alt="${produto.title}">
-      <figcaption><h3>${produto.title}</h3></figcaption>
-    </figure>
-    <p><strong>Preço:</strong> ${produto.price.toFixed(2)} €</p>
-    <p style="background-color: lightgray; border-radius:10px; padding:5px;">${produto.description}</p>
-    <button class="add">+ Adicionar ao Cesto</button>
-  `;
 
-  // Evento do botão de adicionar
-  const botao = artigo.querySelector('.add');
-  botao.addEventListener('click', () => {
-    adicionarAoCesto(produto);
-  });
+     section.innerHTML = "<article>" + data.map(title => {
+       return title + "<br> " + data[0].price + "<br> " + data[0].description + "<br> " + data[0].image + "</article>";
+     }).join("") + "<article>" + data[1].title + "<br> " + data[1].price + "<br> " + data[1].description + "<br> " + data[1].image + "</article>";
 
-  container.appendChild(artigo);
-});
-
-// --- Função para adicionar ao carrinho ---
-function adicionarAoCesto(produto) {
-  const item = document.createElement('li');
-  item.innerHTML = `
-    ${produto.title} - ${produto.price.toFixed(2)} €
-    <button class="remove">Remover</button>
-  `;
-
-  carrinhoLista.appendChild(item);
-  total += produto.price;
-  atualizarTotal();
-
-  const botaoRemover = item.querySelector('.remove');
-  botaoRemover.addEventListener('click', () => {
-    removerDoCesto(item, produto.price);
-  });
-}
-
-// --- Função para remover ---
-function removerDoCesto(item, preco) {
-  item.remove();
-  total -= preco;
-  atualizarTotal();
-}
-
-// --- Atualizar total ---
-function atualizarTotal() {
-  totalElement.textContent = ` Total: ${total.toFixed(2)} €`;
-}
+  })
+  .catch(error => console.error("Error:", error));
