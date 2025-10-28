9ae5fbde0054b6744bcc3e4126d87b7ca505aa44
guilhermecoelho <a22405805@alunos.ulht.pt>
2025-10-28 20:51:02 +0000

    Lab7 Pt.2

--- lab7/scripts.js
+++ lab7/scripts.js
@@ -1,15 +1,62 @@
-const url = "https://deisishop.pythonanywhere.com/products/";
+const urlProdutos = "https://deisishop.pythonanywhere.com/products/";
+const urlCategorias = "https://deisishop.pythonanywhere.com/categories/";
 const section = document.querySelector("#lista-produtos");
+const filtroSelect = document.querySelector("#filtro-categorias");
 
-fetch(url)
+let todosProdutos = []; 
+
+fetch(urlProdutos)
   .then(response => response.json())
-  .then(data =>{
-     console.log(data)
-
-
-     section.innerHTML = "<article>" + data.map(title => {
-       return title + "<br> " + data[0].price + "<br> " + data[0].description + "<br> " + data[0].image + "</article>";
-     }).join("") + "<article>" + data[1].title + "<br> " + data[1].price + "<br> " + data[1].description + "<br> " + data[1].image + "</article>";
-
+  .then(data => {
+    todosProdutos = data;
+    mostrarProdutos(data);
   })
-  .catch(error => console.error("Error:", error));
+  .catch(error => {
+    console.error("Erro ao carregar produtos:", error);
+  });
+
+  
+fetch(urlCategorias)
+  .then(response => response.json())
+  .then(categorias => {
+    categorias.forEach(cat => {
+      const option = document.createElement("option");
+      option.value = cat;
+      option.textContent = cat;
+      filtroSelect.appendChild(option);
+    });
+  })
+  .catch(error => console.error("Erro ao carregar categorias:", error));
+
+function mostrarProdutos(produtos) {
+  section.innerHTML = "";
+  produtos.forEach(produto => {
+    const article = document.createElement("article");
+
+    article.innerHTML = `
+      <figure>
+        <img src="${produto.image}" alt="${produto.title}">
+      </figure>
+      <h3>${produto.title}</h3>
+      <p><b>Preço:</b> ${produto.price} €</p>
+      <p><b>Categoria:</b> ${produto.category.name}</p>
+      <p>${produto.description}</p>
+      <p><b>⭐ ${produto.rating.rate}</b> (${produto.rating.count} avaliações)</p>
+      <button class="add">Adicionar ao Carrinho</button>
+    `;
+    section.appendChild(article);
+  });
+}
+
+filtroSelect.addEventListener("change", () => {
+  const categoriaSelecionada = filtroSelect.value;
+
+  if (categoriaSelecionada === "all") {
+    mostrarProdutos(todosProdutos);
+  } else {
+    const filtrados = todosProdutos.filter(
+      p => p.category.name.toLowerCase() === categoriaSelecionada.toLowerCase()
+    );
+    mostrarProdutos(filtrados);
+  }
+});
